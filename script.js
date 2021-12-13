bbb625612b728cdee3ff8273fad788f25a83aac9
aweliego <amelie.gorry.am@gmail.com>
2021-12-13 23:22:33 +0100

    Rewrote program to accept odd number of participants and to make everyone both secretSanta and elf

--- script.js
+++ script.js
@@ -11,22 +11,30 @@ const names = [
   'Stanley Hudson',
   'Ryan Howard',
   'Kelly Kapoor',
+  'Phyllis Vance',
 ];
 
 const getRandomName = (array) =>
-  names[Math.floor(Math.random() * array.length)];
+  array[Math.floor(Math.random() * array.length)];
 
-const pairParticipants = (array) => {
-  const pairs = [];
-  while (array.length > 0) {
-    let secretSanta = getRandomName(names);
-    array.splice(array.indexOf(secretSanta), 1);
-    let elf = getRandomName(names);
-    array.splice(array.indexOf(elf), 1);
-    pairs.push({ secretSanta: secretSanta, elf: elf });
-  }
-  console.log(pairs);
-  return pairs;
+// Store all the names in 2 different arrays (secretSanta's and elves)
+const secretSantas = names.slice();
+const elves = names.slice();
+
+// Loop over secretSanta's array
+const assignElves = (secretSantas, elves) => {
+  secretSantas.forEach((secretSanta) => {
+    // Randomly pick a name in elves array
+    let randomElf = getRandomName(elves);
+    // If randomElf is the same person as current secretSanta, keep picking a name
+    while (secretSanta === randomElf) {
+      randomElf = getRandomName(elves);
+    }
+    // Remove randomElf from elves array (so it can't be picked again)
+    elves.splice(elves.indexOf(randomElf), 1);
+    // Display secretSanta and their elf
+    console.log(`${secretSanta} is the Secret Santa of ${randomElf}!`);
+  });
 };
 
-pairParticipants(names);
+assignElves(secretSantas, elves);
